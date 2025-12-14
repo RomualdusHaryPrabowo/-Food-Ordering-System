fc320e308467aed1ba6a2d204df4019d5436ecc4
RomualdusHaryPrabowo <romualdushypo@gmail.com>
2025-12-14 22:46:19 +0700

    feat(models): menambah struktur tabel menu

--- backend/app/models.py
+++ backend/app/models.py
@@ -21,6 +21,18 @@ class User(Base):
     def check_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password_hash.encode('utf-8'))
 
+#Model menu
+class MenuItem(Base):
+    __tablename__ = 'menu_items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    category = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
+    image_url = Column(String(255), nullable=True)
+    is_available = Column(Boolean, default=True)
+    description = Column(Text, nullable=True)
+
 def init_db(db_url='sqlite:///food_ordering.db'):
     engine = create_engine(db_url)
     Base.metadata.create_all(engine)
