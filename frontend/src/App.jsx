2266caa91d3348fc752b857b810a2e9c914888ae
RomualdusHaryPrabowo <romualdushypo@gmail.com>
2025-12-16 10:22:35 +0700

    Menambahkan route path menu pada App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Menu from './pages/Menu';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
   );
