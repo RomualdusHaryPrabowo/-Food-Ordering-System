4333e7aa47b901ca7f6cb5063c8ac149390330ea
Hanifah Hasanah <hanifahhasanah50@gmail.com>
2025-12-17 23:15:42 +0700

    feat: add Cart page for customer checkout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} /> 
       </Routes>
     </Router>
   );
