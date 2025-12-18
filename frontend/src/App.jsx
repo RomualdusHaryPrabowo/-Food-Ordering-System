a9caa84cc83008fc4b378bdea103ee6f440f14eb
Hanifah Hasanah <hanifah.123140082@student.itera.ac.id>
2025-12-18 15:40:56 +0700

    feat: tambah route Order History di App.jsx - Hanifah

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
 import Cart from './pages/Cart';
+import OrderHistory from './pages/OrderHistory';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<OrderHistory />} />
       </Routes>
     </Router>
   );
