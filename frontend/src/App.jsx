0bb41d962cd19ad8ab476a106b80cd75ce255e13
Jefri Sembiring <jefri.123140206@student.itera.ac.id>
2025-12-18 08:18:39 +0700

    routing register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
 import Cart from './pages/Cart';
-
+import Register from './pages/Register';  
 function App() {
   return (
     <Router>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/cart" element={<Cart />} /> 
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
